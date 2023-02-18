3739b422a8a3760db4c895e8fe73f5d9466a3f83
yandapanda19 <i.am.kitiket@gmail.com>
2023-02-18 22:41:07 +0200

    add files

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return (<header className="header">
+        <img src="https://www.logodesign.net/images/nature-logo.png" />
+      </header>);
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <main className="content">
+        <div>
+          <img src="https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__480.jpg" />
+        </div>
+      </main>
+    );
+}
+
+export default Profile;
